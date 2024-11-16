7137e76eb011fb40a322dcfce80de4875bf0fd71
000x999 <124853841+000x999@users.noreply.github.com>
2024-11-16 12:10:06 -0500

    Removed intrinsics

--- HashGrid.h
+++ HashGrid.h
@@ -13,8 +13,6 @@
 template <typename T>
 
 struct CollisionCell {
-    //Increased capacity for fewer cache misses..
-    //8 is good enough, can go all the way down to 2 even.
     static constexpr uint8_t m_cellCapacity = 8;
     uint8_t m_objectsCount = 0;
     std::array<T*, m_cellCapacity> m_objects;
@@ -69,29 +67,20 @@ public:
 
     std::vector<std::reference_wrapper<T>> GetPotentialCollisions(const T& obj) const {
         std::vector<std::reference_wrapper<T>> potentialCollisions;
-        //Preallocate to avoid frequent reallocations
-        //I can't seem to find the perfect balance but 100 works well, 20 works good as well
         potentialCollisions.reserve(1000);  
         Vec2 pos = obj.GetPos();
-        //Create the hashing for each cell respectively
-        //Could be done with another hashing function but inlining it in this case seems better
         uint16_t cellX = static_cast<uint16_t>(floor(pos.x / m_cellSize));
         uint16_t cellY = static_cast<uint16_t>(floor(pos.y / m_cellSize));
-        //Defines the starting posistions for the iterators
-        //Adds an extra neighboring cell on each side to check for as well 
-        //Increased collision accuracy at the cost of some performance
         uint16_t minX = std::max(cellX - 1, 0);
         uint16_t maxX = std::min(cellX + 1, static_cast<uint16_t>(m_gridWidth)  - 1);
         uint16_t minY = std::max(cellY - 1, 0);
         uint16_t maxY = std::min(cellY + 1, static_cast<uint16_t>(m_gridHeight) - 1);
-        //Fetches all the potential collisions between particles within a valid range
         for (uint16_t x = minX; x <= maxX; x++) {
             for (uint16_t y = minY; y <= maxY; y++) {
                 uint16_t key = x + y * m_gridWidth;
                 if (key < m_data.size()) {
                     const auto& cell = m_data[key];
                     for (uint8_t i = 0; i < cell.m_objectsCount; ++i) {
-                        //returns a vector of all potential collisions
                         potentialCollisions.push_back(*cell.m_objects[i]);
                     }
                 }
--- Vec2.h
+++ Vec2.h
@@ -1,6 +1,5 @@
 #pragma once
-#include <immintrin.h>
-
+#include <math.h>
 class Vec2
 {
 public:
@@ -12,7 +11,7 @@ public:
 	Vec2& operator*=(float rhs);
 	Vec2 operator-(const Vec2& rhs) const;
 	Vec2& operator-=(const Vec2& rhs);
-	float fast_rsqrt(float x) { return _mm_cvtss_f32(_mm_rsqrt_ss(_mm_set_ss(x))); }
+	float fast_rsqrt(float x) { return sqrt(x); }
 	float fast_sqrt(float x) { return x * fast_rsqrt(x); }
 	float GetLength();
 	float GetLengthSq() const;
